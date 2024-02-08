851f1178778bc0bbf20e15f40b26e84d237fb9f7
Ram Satish Reddy Mallu <ramsatish3330@gmail.com>
2024-02-08 19:32:19 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,110 @@
+let runningTotal = 0;
+let buffer = '0';
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+
+  screen.innerText = buffer;
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case 'C':
+      runningTotal = 0;
+      buffer = '0';
+      break;
+
+    case '=':
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(previousOperator, parseInt(buffer));
+      previousOperator = null;
+      buffer = runningTotal;
+      runningTotal = 0;
+      break;
+
+    case '←':
+      if (buffer.length === 1) {
+        buffer = '0';
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+
+    case '+':
+        handleMath(symbol);
+        break;
+    case '−':
+        handleMath(symbol);
+        break;
+    case '×':
+        handleMath(symbol);
+        break;
+    case '÷':
+      handleMath(symbol);
+      break;
+  }
+}
+
+function handleMath(symbol) {
+  if (buffer === '0') {
+    return;
+  }
+
+  const intbuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intbuffer;
+  } else {
+    flushOperation(previousOperator, intbuffer);
+  }
+  previousOperator = symbol;
+  buffer = '0';
+}
+
+function flushOperation(operator, number) {
+  switch (operator) {
+    case '+':
+      runningTotal += number;
+      break;
+
+    case '−':
+      runningTotal -= number;
+      break;
+
+    case '×':
+      runningTotal *= number;
+      break;
+
+    case '÷':
+      if (number === 0) {
+        return;
+      }
+      runningTotal /= number;
+      break;
+  }
+}
+
+function handleNumber(numberString) {
+  if (buffer === '0') {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+}
+
+function init() {
+  document.querySelector('.calc-buttons').addEventListener('click', function (event) {
+    buttonClick(event.target.innerText);
+  });
+}
+
+init();
